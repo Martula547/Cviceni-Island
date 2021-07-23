aca8215072fe79f833a481db00539d9bf2cabacb
Martina Matejova <martina@nikdo.cz>
2021-07-23 14:37:33 +0200

    ZalohaJavaScript1

---
+++ javascript/mj-script.js
@@ -0,0 +1,9 @@
+let tlacitko = document.querySelector('.moje-tlacitko')
+tlacitko.innerHTML='baf'
+
+let prepniTema = () => {
+    /*alert('Bylo kliknuto')*/
+    document.querySelector('body').classList.toggle('je-svetlo')
+}
+
+tlacitko.addEventListener('click', prepniTema)
